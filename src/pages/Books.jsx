f059448111a4380de8b2e1860caf7eca9b676bbc
Konstantin <kostyakalashnikov607@gmail.com>
2023-05-02 19:31:21 +0200

    t

---
+++ src/pages/Books.jsx
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import "../style.css";
+
+
+const Books = () => {
+    const [books,setBooks] = useState([]);
+
+    useEffect(()=>{
+        
+        const fechAllBooks = async ()=>{
+            try{
+                 const res = await axios.get("http://localhost:8800/books"); 
+                /* const res = await axios.get("http://185.151.245.185/books"); */
+                setBooks(res.data);
+            }catch(err){
+                console.log(err);
+            }
+        }
+        fechAllBooks();
+    },[]);
+
+    /* Функция удаления */
+    const handleDelete = async (id)=>{
+        try{
+            await axios.delete("http://localhost:8800/books/" +id);
+            window.location.reload();
+        }catch(err){
+            console.log(err);
+        }
+    }
+
+    return (
+        <div><h1>Books shop</h1>
+            <div className="books">
+                {books.map((book) => (
+                <div className="book" key={book.id}>
+                    {/* {books.cover && <img src={book.cover} alt="" />} */}
+                    <img src={book.cover} alt="" />
+                    <h2>{book.title}</h2>
+                    <p>{book.desc}</p>
+                    <span>{book.price}</span>
+                    <button className="delete" onClick={()=> handleDelete(book.id)}>Delete</button>
+                   <button className="update"><Link to={`/update/${book.id}`}>Update</Link></button>
+
+                    {/* <button className="update"><Link to={{pathname: `/update/${book.id}`,propsSearch: book.title}}>Update</ Link></button> */}
+                </div>
+                ))}
+                
+            </div>
+            <button>
+                <Link to="/add">Add new book</Link>
+            </button>
+        </div>
+
+    );
+};
+
+export default Books;
---
+++ src/pages/Update.jsx
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useLocation, useNavigate} from "react-router-dom";
+
+
+
+const Update = () => {
+
+
+    const [start,setStart] = useState([]);
+
+    /* useEffect(()=>{
+        const fechAllBooks = async ()=>{
+            try{
+                const res = await axios.get("http://localhost:8800/books/");
+                setStart(res.data); 
+                
+            }catch(err){
+                console.log(err);
+            }
+        }
+        fechAllBooks();
+    },[]); */
+
+
+    const [book,setBook] = useState({
+        title:"",
+        desc:"",
+        price:null,
+        cover:"",
+    });
+
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const bookId = location.pathname.split("/")[2]
+
+    const handleChange = (e) => {
+        setBook((prev) => ({ ...prev, [e.target.name] : e.target.value}));
+    };
+
+    /* Обновление строки */
+    const handleClick = async e =>{
+        e.preventDefault();
+        try{
+             await axios.put("http://localhost:8800/books/" + bookId, book);
+           /* await axios.put("http://185.151.245.185/books/" + bookId, book); */
+            navigate("/");
+        }catch(err){
+            console.log(err);
+        }
+    }
+
+    return (
+        <div className="form">
+            <h1> Update</h1>
+            <input type="text" placeholder="title" /* {bookId} */ onChange={handleChange} name="title" />
+            <input type="text" placeholder="desc" onChange={handleChange} name="desc" />
+            <input type="number" placeholder="price" onChange={handleChange} name="price" />
+            <input type="text" placeholder="cover" onChange={handleChange} name="cover" />
+
+            <button className="formButton" onClick={handleClick}>Update</button>
+        </div>
+    );
+};
+
+export default Update;
